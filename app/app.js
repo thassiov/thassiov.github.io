67320ecb7ffa418543f0b58943ca7889b165ca55
thassiov <tvmcarvalho@gmail.com>
2017-08-31 08:26:41 -0300

    Removendo arquivos do angular

--- app/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('thassiov', ['ui.router', 'thassiov.home'])
-
-.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
-}]);
-
